8f50885c4b956ba529c8462427858e47f69a764f
GOPAL0503 <sayhai2gopal05@gmail.com>
2025-02-04 22:37:05 +0530

    Fix Babel plugin configuration

--- frontend/babel.config.js
+++ frontend/babel.config.js
@@ -2,7 +2,7 @@ module.exports = {
   presets: ["react-app"],
   plugins: [
     "@babel/plugin-proposal-private-methods",
-    "@babel/plugin-proposal-private-property-in-object",
+    "@babel/plugin-proposal-private-property-in-object", // or try @babel/plugin-transform-private-property-in-object
     "@babel/plugin-transform-class-properties",
     "@babel/plugin-transform-nullish-coalescing-operator",
   ],
